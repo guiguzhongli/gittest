f5f5925c38e07b772cc8370d6add86cacf926890
zzh <atguiguzhongli@163.com>
2021-11-24 23:42:12 +0800

    hot-fix commit.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,7 +12,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("22222");
-        System.out.println("33333");
+        System.out.println("33344");
     }
 
 }
