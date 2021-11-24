b933faf11fe1d5db8d703826f80c7c4cb664f4f2
zzh <atguiguzhongli@163.com>
2021-11-25 00:03:39 +0800

    master commit...

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("44444");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("566678");
     }
 
 }
