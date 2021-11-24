4a37b4468d41050a2a50f958b40631361721bea2
zzh <atguiguzhongli@163.com>
2021-11-24 23:36:53 +0800

    thire commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("22222");
+        System.out.println("33333");
     }
 
 }
