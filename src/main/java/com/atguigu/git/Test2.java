5f3fbfc1fce96a600d2fafd19c1e24498d694127
zzh <atguiguzhongli@163.com>
2021-11-24 23:32:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Test2.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2021/11/24 0024 23:31
+ * @Version 1.0
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
