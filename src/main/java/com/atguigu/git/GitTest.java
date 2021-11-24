88393703aa24861df69f6aefe83e41072d103eb5
zzh <atguiguzhongli@163.com>
2021-11-24 23:31:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2021/11/24 0024 23:27
+ * @Version 1.0
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
